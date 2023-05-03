e9d0707e1cd20bc0a170e1fa595a30cef1a52a95
Nate Lentz <npl1@williams.edu>
2023-05-03 10:15:19 -0400

    small edits

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-model = GPT2LMHeadModel.from_pretrained("gpt2")
 
-inputs = tokenizer("Hello, my dog is cute and ", return_tensors="pt")
-generation_output = model.generate(**inputs, max_new_tokens = 1, return_dict_in_generate=True, output_scores=True)
-print(generation_output[:2])
+
+
+
+def main():
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+    model = GPT2LMHeadModel.from_pretrained("gpt2")
+
+    inputs = tokenizer("Hello, my dog is cute and ", return_tensors="pt")
+    generation_output = model.generate(**inputs, max_new_tokens = 1, return_dict_in_generate=True, output_scores=True)
+    print(generation_output[1][0].size())
+
+if __name__ == "__main__":
+    main()
